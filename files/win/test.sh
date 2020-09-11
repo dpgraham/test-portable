50014af8d20b024022c75926c1f5c6311d1edf11
Dan <dpgraham.85@gmail.com>
2020-09-11 16:28:45 -0700

    Again

--- files/win/test.sh
+++ files/win/test.sh
@@ -1,5 +1,5 @@
 echo "Printing Node version"
-./__sauce/node --version
+../__sauce/node --version
 
 echo "Run Cypress verify"
-./node_modules/cypress--v5.1.0/bin/cypress verify
+../node_modules/cypress--v5.1.0/bin/cypress verify
--- scripts/bundle-win.sh
+++ scripts/bundle-win.sh
@@ -2,16 +2,18 @@
 
 # Move files over to ./bundles/bundle-win
 mkdir -p ./bundles/bundle-win/
-cp -r ./files/win/ ./bundles/bundle-win/
+mkdir -p ./bundles/bundle-win/__sauce/
+cp -r ./files/win/. ./bundles/bundle-win/__sauce/
 
 # Install dependencies
 pushd ./bundles/bundle-win/
-ls -lah ./
 mkdir -p ./__sauce/
 mkdir -p ./__sauce/__cache/Cypress/
 cp $(which node) ./__sauce/node
+mv ./__sauce/package.json ./package.json
 npm config set cache $PWD/__sauce/__cache/npm/
 export CYPRESS_CACHE_FOLDER=$PWD/__sauce/__cache/Cypress/
 npm install
-mv package.json 
+rm -rf package.json
+rm -rf package-lock.json
 popd
