78ffafcd4cc1377176c84c2baad4122b3f2f4cff
Dan <dpgraham.85@gmail.com>
2020-09-12 17:57:44 -0700

    Add some docs

--- files/win/test.sh
+++ files/win/test.sh
@@ -2,9 +2,11 @@
 echo "Printing Node version"
 ../__sauce/node --version
 
+# TODO: Make these tests work on Windows. Need Windows workstation.
+
 # Cypress Tests
 echo "Cypress cache location: $CYPRESS_CACHE_FOLDER"
-../__sauce/node.exe ./tests/cypress/verify-cypress.js
+#../__sauce/node.exe ./tests/cypress/verify-cypress.js
 
 ## TestCafe Tests 
 ## NOTE: These tests aren't running on 
