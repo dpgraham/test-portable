8fd6d21d42d31a8303df3ac429ada46a3433d682
Dan Graham <dan@saucelabs.com>
2020-09-12 20:46:12 -0700

    chore: add macOS bundle builder (#4)

--- files/win/test.sh
+++ files/win/test.sh
@@ -9,5 +9,5 @@ echo "Cypress cache location: $CYPRESS_CACHE_FOLDER"
 #../__sauce/node.exe ./tests/cypress/verify-cypress.js
 
 ## TestCafe Tests 
-## NOTE: These tests aren't running on 
+## NOTE: These tests aren't running on
 ##../__sauce/node ./tests/testcafe/run-test-cafe-tests.js
